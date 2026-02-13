e781406d029bcceec34e31e5a3b8cfc63f6a5e72
diegourrego567-lgtm <diegourrego567@gmail.com>
2026-02-13 02:13:13 +0000

    completo

--- src/RegistroTransacciones.java
+++ src/RegistroTransacciones.java
@@ -1,4 +1,3 @@
-
 import java.util.Vector;
 
 
@@ -8,6 +7,18 @@ private Vector<String> logTransacciones;
 
 public RegistroTransacciones(){
     logTransacciones= new Vector<>();
+
+    logTransacciones.add("Transaccion 1: deposito 500");
+    logTransacciones.add("Transaccion 2: retiro 200");
+    logTransacciones.add("Transaccion 3: transfirio 300");
+    logTransacciones.add("Transaccion 4: pago de servicios 150");
+    logTransacciones.add("Transaccion 5: consignacion 1000");
 }
 
+    public void  mostrarLogs () {
+        for(String log: logTransacciones) {
+            System.out.println(log);
+        }
+    }
 }
+
--- src/SistemaBancarioLegacy.java
+++ src/SistemaBancarioLegacy.java
@@ -1,7 +1,42 @@
-import java.util.Vector;
 import java.util.ArrayList;
-
+import java.util.Vector;
 public class SistemaBancarioLegacy {
+    public static void main(String[] args) {
+        
+        medirTiempoVector();
+        medirTiempoArrayList();
+    }
+    public static void medirTiempoVector(){
+        Vector<Integer>vector=new Vector<>();
+
+        long inicio=System.currentTimeMillis();
+
+        for ( int i =0 ; i< 1000000;i++) {
+            vector.add(i);
+            
+        }
+        long fin=System.currentTimeMillis();
+        System.out.println("tiempo vector:"+(fin-inicio)+"ms");
+
+    }
+
+    public static void medirTiempoArrayList() {
+
+        ArrayList<Integer>arrayList=new ArrayList<>();
+
+        long inicio= System.currentTimeMillis();
+
+        for ( int i =0 ; i< 1000000;i++) {
+            arrayList.add(i);
+            
+        
+    }
+
+    long fin= System.currentTimeMillis();
+    System.out.println("tiempo ArrayList: "+ (fin-inicio)+ "ms");
+
+
+    }
     
 }
 
